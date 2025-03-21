0447113c111e5283c7f58af6e726e661bb35b31a
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2025-03-21 14:55:54 +0900

    Add files via upload

---
+++ ArcTextEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace ArcText
+{
+    [VideoEffect("アーチ配置", ["配置"], ["arch","a-tihaiti","ArTex"], isAviUtlSupported:false)]
+    internal class ArcTextEffect: VideoEffectBase
+    {
+        public override string Label => "アーチ配置";
+
+        [Display(GroupName = "配置", Name = "高さ", Description = "高さ")]
+        [AnimationSlider("F1", "px", -100, 100)]
+        public Animation Height { get; } = new Animation(-100, -99999, 99999);
+
+        [Display(GroupName = "配置", Name = "中心位置", Description = "X座標の中心位置")]
+        [AnimationSlider("F1", "px", -100, 100)]
+        public Animation CenterXPoint { get; } = new Animation(0, -99999, 99999);
+
+        [Display(GroupName = "配置", Name = "角度", Description = "角度")]
+        [AnimationSlider("F1", "°", -90, 90)]
+        public Animation Angle { get; } = new Animation(0, -3600, 3600);
+
+        [Display(GroupName = "配置", Name = "間隔", Description = "間隔")]
+        [AnimationSlider("F1", "px", -100, 100)]
+        public Animation Interval { get; } = new Animation(0, -99999, 99999);
+
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return[];
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new ArcTextEffectProcessor(devices, this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [Height, Interval, Angle, CenterXPoint];
+    }
+}
---
+++ ArcTextEffectProcessor.cs
@@ -0,0 +1,86 @@
+﻿using Vortice.Direct2D1;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+
+namespace ArcText
+{
+    internal class ArcTextEffectProcessor : IVideoEffectProcessor
+    {
+        readonly ArcTextEffect item;
+        ID2D1Image? input;
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + " is null");
+
+        public ArcTextEffectProcessor(IGraphicsDevicesAndContext devices, ArcTextEffect item)
+        {
+            this.item = item;
+        }
+
+
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+
+            var X = effectDescription.DrawDescription.Draw.X;
+            var Y = effectDescription.DrawDescription.CenterPoint.Y;
+            var textIndex = effectDescription.InputIndex;
+            var totalTextCount = effectDescription.InputCount;
+
+            var height = item.Height.GetValue(frame, length, fps);
+            var angleIntensity = item.Angle.GetValue(frame, length, fps);
+            var interval = item.Interval.GetValue(frame, length, fps);
+            var centerXPoint = item.CenterXPoint.GetValue(frame, length, fps);
+
+
+            float t = 0.0f;
+            if (totalTextCount > 2)
+            {
+                t = (textIndex - (totalTextCount - 1) / 2.0f) / ((totalTextCount - 1) / 2.0f);
+            }
+            
+
+            float distanceFactor = MathF.Cos(MathF.PI * (t - (float)centerXPoint / 400.0f) / 2.0f );
+            float y = (float)height * distanceFactor;
+
+
+            float dx = MathF.Abs(X) + MathF.Abs((float)centerXPoint);
+            float dy = Y + y  + (float)height;
+            
+            float angle = (MathF.Atan2(dy, dx) * 180.0f / MathF.PI + (float)angleIntensity * MathF.Abs(t)) * -(t - (float)centerXPoint / 400.0f);
+
+
+            var drawDesc = effectDescription.DrawDescription;
+            return drawDesc with
+            {
+                Draw = new(
+                    drawDesc.Draw.X + (float)interval * t,
+                    drawDesc.Draw.Y + y,
+                    drawDesc.Draw.Z
+                ),
+                Rotation = new(
+                    drawDesc.Rotation.X,
+                    drawDesc.Rotation.Y,
+                    drawDesc.Rotation.Z + angle
+                )
+            };
+        }
+
+
+
+
+        public void ClearInput()
+        {
+            input = null;
+        }
+
+        public void SetInput(ID2D1Image? input)
+        {
+            this.input = input;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
